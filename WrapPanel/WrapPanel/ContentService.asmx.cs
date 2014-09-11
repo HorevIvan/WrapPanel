eceb24a247333b24edad288dff41608e94c25677
horevivan <horevivan@yandex.ru>
2014-09-11 17:38:10 +0800

    Добавлено получние данных с сервиса

---
+++ WrapPanel/WrapPanel/ContentService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WrapPanel
+{
+    /// <summary>
+    /// Summary description for ContentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class ContentService : System.Web.Services.WebService
+    {
+        private static readonly Random Random = new System.Random();
+
+        [WebMethod]
+        public List<ContentItem> GetContent()
+        {
+            var items = new List<ContentItem>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var item = new ContentItem();
+
+                var count = Random.Next(10, 400);
+
+                for (var j = 0; j < count; j++)
+                {
+                    item.Html += j + " ";
+                }
+
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+    
+    [Serializable]
+    public class ContentItem
+    {
+        public String Html { set; get; }
+    }
+}
---
+++ WrapPanel/WrapPanel/scripts/content.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="jquery-2.1.1.js" />
+
+function getContainer() {
+    return $('#container');
+}
+
+function fillContent(onComplete)
+{
+    $.ajax({
+        type: "POST",
+        url: "/ContentService.asmx/GetContent",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data, status, req, xml, xmlHttpRequest, responseXML) {
+            var contentItems = data.d;
+            var container = getContainer();
+            container.empty();
+            $.each(contentItems, function (index, contentItem) {
+                container.append("<div class='contentBlock'>" + contentItem.Html + "</div>");
+            });
+            onComplete();
+        },
+        error: function (data, status, req, xml, xmlHttpRequest, responseXML) {
+        }
+    });
+}
